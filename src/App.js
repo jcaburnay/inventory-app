d086abb88efacda5810fec0afdfaf89152d1e2be
nongbi <jmcaburnay1@up.edu.ph>
2021-01-13 22:32:32 +0800

    Multiple inventories

--- src/App.js
+++ src/App.js
@@ -3,6 +3,52 @@ import { useProducts } from './context';
 import { useInput } from './custom-hooks';
 import "./App.css";
 
+//Inventory Components
+
+function AddInventory() {
+  const { addInventory } = useProducts();
+  const [inventory, setInventory] = useInput("");
+  const [description, setDescription] = useInput("");
+
+  const submit = e => {
+    e.preventDefault();
+    addInventory(inventory.value, description.value);
+    setDescription();
+    setInventory();
+  }
+
+  return (
+    <>
+      <form onSubmit={submit}>
+        <label>
+          Inventory name:
+          <input {...inventory} type="text" placeholder="inventory name" required/>
+        </label>
+        <br />
+        <label>
+          Description:
+          <input {...description} type="text" placeholder="description" required/>
+        </label>
+        <br />
+        <button>Add inventory</button>
+      </form>
+      <br />
+    </>
+  )
+}
+
+function InventoryList() {
+  const { inventories, handleInventoryItems } = useProducts();
+  return (
+    <>
+      <div>
+        {inventories.length === 0 ? <div>No inventory listed.</div> : inventories.map(inventory => <button key={inventory.id} onClick={() => handleInventoryItems(inventory.id)}>{inventory.name}</button>)}
+      </div>
+      <hr />
+    </>
+  )
+}
+
 function AddItem() {
   const { addItem } = useProducts();
   const [name, setName] = useInput("");
@@ -10,34 +56,62 @@ function AddItem() {
   const [sellPrice, setSellPrice] = useInput("");
   const [quantity, setQuantity] = useInput("");
   const [category, setCategory] = useInput("");
-  const [locations, setLocations] = useInput(""); 
-  const submit = e => {
-    e.preventDefault();
-    addItem(name.value, Number(purchasePrice.value), Number(sellPrice.value), Number(quantity.value), category.value, locations.value);
+
+  const resetInput = () => {
     setName();
     setPurchasePrice();
     setSellPrice();
     setQuantity();
     setCategory();
-    setLocations();
-  }
+  };
+
+  const submit = e => {
+    e.preventDefault();
+    addItem(name.value, Number(purchasePrice.value), Number(sellPrice.value), Number(quantity.value), category.value);
+    resetInput();
+  };
+
   return (
-    <form onSubmit={submit}>
-      <input {...name} type="text" placeholder="Name" required />
-      <input {...purchasePrice} type="number" placeholder="Purchase Price" min="0" required/>
-      <input {...sellPrice} type="number" placeholder="Sell Price" min="0" required/>
-      <input {...quantity} type="number" placeholder="Quantity" min="1" required/>
-      <input {...category} type="text" placeholder="Category" required/>
-      <input {...locations} type="text" placeholder="Location" required/>
-      <button>Add item</button>
-    </form>
+    <>
+      <form onSubmit={submit}>
+        <label>
+          Name:
+          <input {...name} type="text" placeholder="product name" required />
+        </label>
+        <br />
+        <label>
+          Price (Purchase):
+          <input {...purchasePrice} type="number" placeholder="purchasing price" min="0" required/>
+        </label>
+        <br />
+        <label>
+          Price (Sell):
+          <input {...sellPrice} type="number" placeholder="selling price" min="0" required/>
+        </label>
+        <br />
+        <label>
+          Quantity:
+          <input {...quantity} type="number" placeholder="quantity" min="1" required/>
+        </label>
+        <br />
+        <label>
+          Category:
+          <input {...category} type="text" placeholder="product category" required/>
+        </label>
+        <br />
+        <button>Add item</button>
+      </form>
+      <br />
+    </>
+    
   )
 }
 
 function Product({ id, name, price, quantity }) {
-  const { handleDecrement, handleIncrement } = useProducts();
+  const { handleRemove, handleDecrement, handleIncrement } = useProducts();
   return (
     <li key={id}>
+      <button onClick={() => handleRemove(id)}>x</button>
       {name} - PHP{price.sellPrice} x{quantity}
       <button onClick={() => handleDecrement(id, quantity)}>-</button>
       <button onClick={() => handleIncrement(id, quantity)}>+</button>
@@ -46,10 +120,23 @@ function Product({ id, name, price, quantity }) {
 }
 
 function ProductList() {
-  const { products } = useProducts();
+  const { products, setProducts } = useProducts();
+  const handleFilter = category => {
+    if(category === 'all') {
+      setProducts(products);
+    } else {
+      setProducts(products.filter(product => product.category === category));
+    }
+  };
+
+  const categories = ['all'].concat(products.map(product => product.category).filter((category, idx, arr) => arr.indexOf(category) === idx));
+
   if (!products.length) return <div>No product listed.</div>;
   return (
     <div>
+      <div>
+        {categories.map((category, idx) => <button key={idx} onClick={() => handleFilter(category)}>{category}</button>)}
+      </div>
       <ul>
         {products.map(product => (
           <Product
@@ -65,6 +152,8 @@ function ProductList() {
 function App() {
   return (
     <>
+      <AddInventory />
+      <InventoryList />
       <AddItem />
       <ProductList />
     </>
--- src/context.js
+++ src/context.js
@@ -1,17 +1,120 @@
-import React, { useState, useContext, createContext } from 'react';
-import productData from './product-data.json';
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import { v4 } from 'uuid';
 
-let id = 3;
-function generateId() {
-  id++;
-  return id;
-};
+let initialInventories = [
+  {
+    id: v4(),
+    name: 'Gadgets',
+    description: 'Room storage for devices/gadgets',
+    products: [
+      {
+        id: v4(),
+        name: 'Mechanical Keyboard TKL',
+        price: {
+          purchasePrice: 2550,
+          sellPrice: 2950
+        },
+        quantity: 4,
+        category: 'Computer Accessories'
+      },
+      {
+        id: v4(),
+        name: 'Mouse',
+        price: {
+          purchasePrice: 879,
+          sellPrice: 999
+        },
+        quantity: 7,
+        category: 'Computer Accessories'
+      },
+    ]
+  },
+  {
+    id: v4(),
+    name: 'Food Storage',
+    description: 'Storage for foods',
+    products: [
+      {
+        id: v4(),
+        name: 'Milo',
+        price: {
+          purchasePrice: 89,
+          sellPrice: 109
+        },
+        quantity: 24,
+        category: 'Drinks'
+      },
+      {
+        id: v4(),
+        name: 'Bear Brand',
+        price: {
+          purchasePrice: 76,
+          sellPrice: 89
+        },
+        quantity: 36,
+        category: 'Drinks'
+      },
+      {
+        id: v4(),
+        name: 'Ladys Choice',
+        price: {
+          purchasePrice: 99,
+          sellPrice: 129
+        },
+        quantity: 3,
+        category: 'Spread'
+      },
+      {
+        id: v4(),
+        name: '555 Fried Sardines',
+        price: {
+          purchasePrice: 22,
+          sellPrice: 34
+        },
+        quantity: 12,
+        category: 'Canned goods'
+      },
+    ]
+  }
+];
 
 const ProductContext = createContext();
 const useProducts = () => useContext(ProductContext);
 
 function ProductProvider({ children }) {
-  const [products, setProducts] = useState(productData);
+  const localInventory = localStorage.getItem('inventories');
+  const [inventories, setInventories] = useState(!localInventory ? JSON.parse(localInventory) : initialInventories);
+  const [products, setProducts] = useState([]);
+
+  // useEffect(() => {
+  //   localStorage.setItem('products', JSON.stringify(products))
+  // }, [products]);
+
+  useEffect(() => {
+    localStorage.setItem('inventories', JSON.stringify(inventories))
+  }, [inventories]);
+
+  const addInventory = (name, description) => {
+    setInventories([
+      ...inventories,
+      {
+        "id": v4(),
+        "name": name,
+        "description": description,
+        "products": []
+      }
+    ])
+  };
+
+  const handleInventoryItems = id => {
+    if(!initialInventories.filter(inventory => inventory.id === id).length) {
+      setProducts([]);
+    } else {
+      let products = initialInventories.filter(inventory => inventory.id === id)[0].products.map(product => product);
+      setProducts(products);
+    }
+  };
+
   const handleQuantityChange = (id, quantity) => {
     setProducts(
       products.map((product) =>
@@ -19,10 +122,12 @@ function ProductProvider({ children }) {
       )
     );
   };
+
   const handleIncrement = (id, quantity) => {
     quantity = quantity + 1;
     handleQuantityChange(id, quantity);
   };
+
   const handleDecrement = (id, quantity) => {
     if(quantity === 0) {
       return;
@@ -30,28 +135,30 @@ function ProductProvider({ children }) {
     quantity = quantity - 1;
     handleQuantityChange(id, quantity);
   };
-  const addItem = (name, purchasePrice, sellPrice, quantity, category, locations) => {
+
+  const addItem = (name, purchasePrice, sellPrice, quantity, category) => {
     setProducts([
       ...products,
       {
-        "id": generateId(),
+        "id": v4(),
         "name": name,
         "price": { 
           "purchasePrice": purchasePrice,
           "sellPrice": sellPrice
         },
         "quantity": quantity,
-        "category": category,
-        "locations": [ 
-          locations
-        ]
+        "category": category
       }
     ]);
-  }
-    
+  };
+
+  const handleRemove = id => {
+    setProducts(products.filter(product => product.id !== id));
+  };
+
   return (
-    <ProductContext.Provider value={{ products, handleIncrement, handleDecrement, addItem }}>
-      {children}
+    <ProductContext.Provider value={{ addInventory, handleInventoryItems, inventories, products, setProducts, handleIncrement, handleDecrement, addItem, handleRemove }}>
+      { children }
     </ProductContext.Provider>
   );
 }
